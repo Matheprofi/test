2b128e9586252c6fa556fc8d577bad99e5efa611
Christian Mueller <c.Mueller31@gmx.de>
2015-05-30 17:24:44 +0200

    asdadaad

--- lokal1test1/src/lokal1test1/starte.java
+++ lokal1test1/src/lokal1test1/starte.java
@@ -5,7 +5,6 @@ public class starte {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Peter/test");
 		System.out.println("test3)");
 		System.out.println("Test");
 		System.out.println("test3)");
