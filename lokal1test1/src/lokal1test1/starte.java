5c177e471a4ec9ea55582f6675ce25f915feceb0
Christian Mueller <c.Mueller31@gmx.de>
2015-05-30 16:51:26 +0200

    test am ende

--- lokal1test1/src/lokal1test1/starte.java
+++ lokal1test1/src/lokal1test1/starte.java
@@ -6,10 +6,7 @@ public class starte {
 		// TODO Auto-generated method stub
 
 		System.out.println("Peter/test");
-		System.out.println("TesrInDevelop");
-		System.out.println("test3)");
-		System.out.println("test5)");
-		System.out.println("testtest");
+		System.out.println("Test");
 	}
 
 }
